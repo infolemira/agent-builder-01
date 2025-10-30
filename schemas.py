71ca180641b3a7c19cb15afeca2c9ba2eb3b7ebd
infolemira <infolemira@gmail.com>
2025-10-30 11:46:57 +0100

    Update schemas.py

--- schemas.py
+++ schemas.py
@@ -1,5 +1,30 @@
-from pydantic import BaseModel, EmailStr
+# schemas.py
+from typing import Optional
+from pydantic import BaseModel, Field, EmailStr
 
-class ClientIn(BaseModel):
-    name: str
+# -----------------------------
+# Item sheme
+# -----------------------------
+class ItemCreate(BaseModel):
+    title: str = Field(..., min_length=1)
+    description: Optional[str] = None
+    done: bool = False
+
+
+class ItemUpdate(BaseModel):
+    title: Optional[str] = Field(None, min_length=1)
+    description: Optional[str] = None
+    done: Optional[bool] = None
+
+
+# -----------------------------
+# Auth sheme
+# -----------------------------
+class LoginPayload(BaseModel):
     email: EmailStr
+    password: str = Field(..., min_length=6)
+
+
+class SignupPayload(BaseModel):
+    email: EmailStr
+    password: str = Field(..., min_length=6)
